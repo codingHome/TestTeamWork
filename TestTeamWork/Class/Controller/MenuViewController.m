d7c17b7ed48bc5face7a602f88caff39d048bca5
juvenile-TA <ylibang@hotmail.com>
2014-12-30 20:07:24 +0800

    侧边栏

--- TestTeamWork/Class/Controller/MenuViewController.m
+++ TestTeamWork/Class/Controller/MenuViewController.m
@@ -7,23 +7,37 @@
 //
 
 #import "MenuViewController.h"
-
+#import "ShareViewController.h"
 @interface MenuViewController ()
 @property (nonatomic, strong)UITableView* myTableView;
+@property (nonatomic ,strong) NSArray* cellList1  ;
+@property (nonatomic ,strong) NSArray* cellList2  ;
 @end
 
 @implementation MenuViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blackColor];
+    //self.view.backgroundColor = [UIColor blackColor];
     self.myTableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStyleGrouped];
+    self.myTableView.backgroundColor = [UIColor blackColor] ;
     [self.myTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:identifier];
     self.myTableView.delegate = self;
     self.myTableView.dataSource = self;
     [self.view addSubview:self.myTableView];
+    self.cellList1 = @[@"分享",@"编辑地点",@"目前位置"];
+    self.cellList2 = @[@"设置",@"意见和建议",@"给此应用程序打分"];
 }
-
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    switch (section) {
+        case 0:
+            return nil ;
+        case 1:
+            return @"工具" ;
+        default:
+            return @"Unknown";
+    }
+}
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 2;
@@ -31,7 +45,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if (section == 0) {
-        return 2;
+        return 3;
     } else {
         return 3;
     }
@@ -40,7 +54,26 @@ static NSString *identifier = @"cell";
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
-    cell.textLabel.text = @"11";
+    //cell.textLabel.text = @"11";
+    cell.backgroundColor = [UIColor darkGrayColor] ;
+    [cell.textLabel setTintColor:[UIColor whiteColor]] ;
+    switch (indexPath.section) {
+        case 0:
+            // cell.imageView. = [UIColor blackColor] ;
+            cell.textLabel.text = self.cellList1[indexPath.row] ;
+            break ;
+        case 1:
+            cell.textLabel.text = self.cellList2[indexPath.row] ;
+            break ;
+        default:
+            cell.textLabel.text = @"Unknown" ;
+    }
     return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    ShareViewController* share = [[ShareViewController alloc]init ] ;
+    [self presentModalViewController:share animated:YES];
+    
+    
 }
 @end
---
+++ TestTeamWork/Class/Controller/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  TestTeamWork
+//
+//  Created by 李棒 on 14-12-30.
+//  Copyright (c) 2014年 GE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareViewController : UIViewController
+
+@end
